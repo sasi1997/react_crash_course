e2e072c63d9d6dabc9640193813ad21dbef594dc
sasi kumar <sasikumar@evvoiot.com>
2023-04-07 10:04:02 +0530

    worked on moveland website

--- react_crash_course/src/App.js
+++ react_crash_course/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import { useEffect } from "react";
 import './App.css';
+import SearchIcon from './search.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// 9ad34af
+const API_URL = 'http://www.omdbapi.com?apikey=9ad34af'
+
+const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        console.log(data.Search,'-<<, data')
+    }
+    useEffect(() => {
+        searchMovies('spiderman');
+    },[])
+    return(
+        <div className="App">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input placeholder="Search for movies" 
+                    value= "spiderman" 
+                    onClick={() => {}} 
+                    />
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
